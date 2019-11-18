41b9df6afb9dde5d8bbee91ced6d51fecec7c27e
Andrew Prasetya <andrewprasetya@andrew-imac.local>
2019-11-17 21:50:31 -0500

    Got rollup to compile properly.

--- rollup.config.js
+++ rollup.config.js
@@ -1,10 +1,10 @@
 import typescript from 'rollup-plugin-typescript';
-import resolve from 'rollup-plugin-node-resolve';
 import babel from 'rollup-plugin-babel';
 import commonjs from 'rollup-plugin-commonjs';
 import alias from 'rollup-plugin-alias';
 import pkg from './package.json';
 import { terser } from "rollup-plugin-terser";
+import resolve from 'rollup-plugin-node-resolve';
 
 export default {
   input: 'stack-up/stack-up.ts',
